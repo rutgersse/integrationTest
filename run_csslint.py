6d88831693d531682c8eddec084010b5623d6aa8
Harsh Kothari <harshkothari410@gmail.com>
2015-12-04 05:21:40 -0500

    initial commit

---
+++ run_csslint.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8; mode: django -*-
+import os
+import subprocess
+import codecs
+from optparse import make_option
+from django.conf import settings
+from django_jenkins.tasks import static_files_iterator
+
+
+class Reporter(object):
+    # TODO Remove, when drop django 1.7 support
+    option_list = (
+        make_option("--csslint-exclude",
+                    dest="csslint_exclude", default=".min.css",
+                    help="Comma separated exclude file patterns"),
+        make_option("--csslint-ignore",
+                    dest="csslint_ignore", default="",
+                    help="CSSLint Ignore rules")
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument("--csslint-exclude",
+                            dest="csslint_exclude", default=".min.css",
+                            help="Comma separated exclude file patterns")
+        parser.add_argument("--csslint-ignore",
+                            dest="csslint_ignore", default="",
+                            help="CSSLint Ignore rules")
+
+    def run(self, apps_locations, **options):
+        output = codecs.open(os.path.join(options['output_dir'], 'csslint.report'), 'w', 'utf-8')
+
+        files = list(
+            static_files_iterator(apps_locations + list(getattr(settings, 'STATICFILES_DIRS', [])),
+                                  extension='.css',
+                                  ignore_patterns=options['csslint_exclude'].split(',')))
+
+        if files:
+            cmd = ['csslint', '--format=lint-xml'] + files
+
+            if options['csslint_ignore']:
+                cmd += ['--ignore=%s' % options['csslint_ignore']]
+
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            process_output, err = process.communicate()
+            retcode = process.poll()
+            if retcode not in [0, 1]:  # normal csslint return codes
+                raise subprocess.CalledProcessError(retcode, cmd, output=output + '\n' + err)
+
+            output.write(process_output.decode('utf-8'))
+        else:
+            output.write('<?xml version="1.0" encoding='
+                         '"utf-8"?><lint></lint>')
+
+        output.close()
---
+++ run_jshint.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import os
+import codecs
+import subprocess
+from optparse import make_option
+from django.conf import settings
+from django_jenkins.tasks import static_files_iterator
+
+
+class Reporter(object):
+    # TODO Remove, when drop django 1.7 support
+    option_list = (
+        make_option("--jshint-exclude",
+                    dest="jshint_exclude", default="",
+                    help="Exclude patterns"),
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument("--jshint-exclude",
+                            dest="jshint_exclude", default="",
+                            help="Exclude patterns")
+
+    def run(self, apps_locations, **options):
+        output = codecs.open(os.path.join(options['output_dir'], 'jshint.xml'), 'w', 'utf-8')
+
+        files = list(
+            static_files_iterator(apps_locations + list(getattr(settings, 'STATICFILES_DIRS', [])),
+                                  extension='.js',
+                                  ignore_patterns=options['jshint_exclude'].split(',')))
+
+        cmd = ['jshint', '--reporter=jslint'] + files
+
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        jshint_output, err = process.communicate()
+        retcode = process.poll()
+        if retcode not in [0, 1, 2]:  # normal jshint return codes
+            raise subprocess.CalledProcessError(retcode, cmd, output='%s\n%s' % (jshint_output, err))
+
+        output.write(jshint_output.decode('utf-8'))
+        output.close()
